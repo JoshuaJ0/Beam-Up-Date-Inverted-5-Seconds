7a8f3c0158fecbc2c40f009b24878c91a8074c15
Joshua J <joshua@joshuajiang.me>
2014-01-29 21:29:59 -0600

    First Commit Pebble Face
    First Commit of my Pebble Face

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 2, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "BeamUpInvDate5Sec",
+  .company = "Joshua Jiang",
+  .icon_resource_id = RESOURCE_ID_IMAGE_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = APP_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x08, 0x5B, 0x5F, 0xE7, 0xFC, 0xCE, 0x4F, 0xEF, 0xAA, 0x88, 0x22, 0x26, 0x95, 0xCF, 0x75, 0xDE },
+  .virtual_size = 0xb6b6
+};
